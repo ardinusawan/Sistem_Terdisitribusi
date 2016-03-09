9bd0a16c244b0b39ac63c5f742a3616977f78223
Dwi <arijayadwi@gmail.com>
2016-03-09 22:23:57 +0700

    Sudah bisa rekap perhitungan

--- RPC/multiCallClient.py
+++ RPC/multiCallClient.py
@@ -1,22 +1,55 @@
+__author__ = 'Indra Gunawan'
 import xmlrpclib
+def unzip(args):
+    """
+    inverse of zip. Given: ((1,"a"),(2,"b")) --> ((1,2),("a","b"))
+
+    seq == unzip(zip(seq)) if seq is a rectangular matrix (all of its row has the same length.
+    """
+    result = []
+    n = min(map(len,args))
+    for i in range(n):result.append([])
+    for i in range(len(args)):
+        for j in range(n):
+            result[j].append(args[i][j])
+    return tuple(result)
+
 
 proxy = xmlrpclib.ServerProxy("http://localhost:8000/", allow_none=True)
+#proxy2 = xmlrpclib.ServerProxy("http://localhost:8000/", allow_none=True)
 multicall = xmlrpclib.MultiCall(proxy)
-multicall.add(2, 3)
+#multicall.add(2, 3)
 # multicall.subtract(2, 3)
 # multicall.multiply(2, 3)
 # multicall.divide(2.0, 3.0)
 ##server mencari pc yg terhubung
 ##server membagi tugas
 
-multicall.SplitElementTxt("cron.txt")
-multicall.SortCount()
-##server mendapatkan return value
-result = multicall()
-print tuple(result)
+multicall.SplitElementTxt("cron")
+multicall.SplitElementTxt("cron.1")
+multicall.SplitElementTxt("cron.2")
 
-multicall.SplitElementTxt("coba.txt")
-multicall.SortCount()
+#multicall.SortCount()
 ##server mendapatkan return value
 result = multicall()
+# for i in range((result)):
+#     x,y=zip(*result)
+# xy=(list(x),list(y))
+#
+# list1, list2 = unzip((*result)
+# list(result) == zip(list1, list2)
+
+# list1= ( x[0] for x in result )
+# list2= ( x[1] for x in result )
+#
+# print str(list1), str(list2)
+
+
+# zip(result)
+# print tuple(zip(result))
 print tuple(result)
+#multicall.SplitElementTxt("coba.txt")
+#multicall.SortCount()
+##server mendapatkan return value
+#result = multicall()
+#print tuple(result)
