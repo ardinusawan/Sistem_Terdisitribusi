40135b3763b751309259af33b3cea4068d728bfa
ardinusawan <ardi.nusawan13@gmail.com>
2016-03-10 00:02:33 +0700

    RPC 1.0 -manajer blum fetch data

---
+++ RPC/Manajer/multiCallClient.py
@@ -0,0 +1,61 @@
+__author__ = 'Indra Gunawan'
+import xmlrpclib
+"""
+def unzip(args):
+
+    inverse of zip. Given: ((1,"a"),(2,"b")) --> ((1,2),("a","b"))
+
+    seq == unzip(zip(seq)) if seq is a rectangular matrix (all of its row has the same length.
+
+    result = []
+    n = min(map(len,args))
+    for i in range(n):result.append([])
+    for i in range(len(args)):
+        for j in range(n):
+            result[j].append(args[i][j])
+    return tuple(result)
+"""
+
+proxy = xmlrpclib.ServerProxy("http://localhost:8000/")
+#proxy2 = xmlrpclib.ServerProxy("http://localhost:8000/", allow_none=True)
+multicall = xmlrpclib.MultiCall(proxy)
+folder_hasil_komputasi = "Hasil_Server/"
+#multicall.add(2, 3)
+# multicall.subtract(2, 3)
+# multicall.multiply(2, 3)
+# multicall.divide(2.0, 3.0)
+##server mencari pc yg terhubung
+##server membagi tugas
+
+multicall.SplitElementTxt("cron")
+multicall.SplitElementTxt("cron.1")
+multicall.SplitElementTxt("cron.2")
+
+#multicall.SortCount()
+##server mendapatkan return value
+result = multicall()
+
+
+# for i in range((result)):
+#     x,y=zip(*result)
+# xy=(list(x),list(y))
+#
+# list1, list2 = unzip((*result)
+# list(result) == zip(list1, list2)
+
+# list1= ( x[0] for x in result )
+# list2= ( x[1] for x in result )
+#
+# print str(list1), str(list2)
+
+
+# zip(result)
+# print tuple(zip(result))
+print tuple(result)
+with open(folder_hasil_komputasi + "fetched_python_data.txt", "wb") as handle:
+    handle.write(proxy.datakirim().data)
+#multicall.SplitElementTxt("coba.txt")
+#multicall.SortCount()
+##server mendapatkan return value
+#result = multicall()
+#print tuple(result)
---
+++ RPC/Server/multiCallServer.py
@@ -0,0 +1,148 @@
+__author__ = 'Indra Gunawan'
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import xmlrpclib
+import re
+import collections
+
+temp3=[]
+tempc=[]
+tempoftemp = []
+tempoftimec = []
+hit = 0
+flag=0
+folder_hasil_computasi="Hasil_Computasi/"
+def SplitElementTxt(ofile):
+    global folder_hasil_computasi, flag, temp3, hit, tempc, tempoftemp, tempoftimec
+    temp1=[]
+    temp_count=[]
+    folder_log="Log/"
+
+    ofile = folder_log + ofile
+    buka = open(ofile)
+    for i, line in enumerate(buka):
+        lol = re.split("\W+", line, 8)
+        temp1.append('(' + lol[8])
+    f = open(folder_hasil_computasi + "cron-copy.txt", 'wb')
+    f.writelines(temp1)
+    buka.close()
+    temp2=[]
+    temp_count=[]
+    with open(folder_hasil_computasi + "cron-copy.txt") as infile:
+        counts = collections.Counter(l.strip() for l in infile)
+    for line, count in counts.most_common():
+        temp2.append(line)
+        temp_count.append(count)
+        # return line, count
+    infile.close()
+    #tempoftemp.append([temp2, temp_count])
+    #buka2 = open(ofile)
+    '''
+    fmt = '%-8s%-20s%s'
+    print(fmt % ('',  'Frequent','Command'))
+    fole = open("server1.txt", 'a')
+    for i, (name, grade) in enumerate(zip(temp_count,temp2)):
+        #print(fmt % (i, name, grade))
+        data3 = fmt % (i, name, grade)
+        #print data3
+        fole.write(data3+"\n")
+
+    buka2.close()
+    '''
+    if hit == 0 :
+        temp3 = temp2
+        tempc = temp_count
+        hit=hit +1
+    else :
+        tempoftemp = temp2
+        tempoftempc = temp_count
+        hit=hit +1
+
+    #lola = temp + " "
+    #lolu = lola + str(temp_count)
+    #return lolu
+    #print tempoftemp
+    iter1 = 0
+    iter2 = 0
+    if hit>1 :
+        '''
+        for word in tempoftemp:
+            #print word
+            if word in kuda:
+                for word2 in kuda :
+                    if word == word2:
+                        tempc[iter2] += tempoftempc[iter1]
+                    else :
+                        iter2+=1
+                iter1+=1
+            else :
+                kuda.append(tempoftemp[iter1])
+                tempc.append(tempoftempc[iter1])
+                iter1+=1
+            '''
+        lentemp = len(tempoftemp)
+        lentemp3 = len(temp3)
+        #print nyonyo
+        cek = 0
+        for i in range(lentemp):
+            for j in range(lentemp3):
+                cek+=1
+                if tempoftemp[i] == temp3[j]:
+                    tempc[j] += tempoftempc[i]
+                    cek = -10;
+            if cek==lentemp3-1 :
+                temp3.append(tempoftemp[i])
+                tempc.append(tempoftempc[i])
+            cek = 0
+
+    buka2 = open(ofile)
+    fmt = '%-8s%-20s%s'
+    print(fmt % ('',  'Frequent','Command'))
+    fole = open(folder_hasil_computasi +  "server1.txt", 'w')
+    for i, (name, grade) in enumerate(zip(tempc,temp3)):
+        #print(fmt % (i, name, grade))
+        data3 = fmt % (i, name, grade)
+        #print data3
+        fole.write(data3+"\n")
+
+    buka2.close()
+
+
+    '''
+    zipp = zip (temp3, tempc)
+
+    flag+=1
+    if(flag%3):
+        print zipp
+        return zipp
+
+    print flag;
+    '''
+
+'''
+def SortCount():
+    temp=[]
+    temp_count=[]
+    with open('cron-copy.txt') as infile:
+        counts = collections.Counter(l.strip() for l in infile)
+    for line, count in counts.most_common():
+        temp.append(line)
+        temp_count.append(count)
+        # return line, count
+    infile.close()
+    return temp, temp_count
+'''
+def datakirim():
+    with open(folder_hasil_computasi + "server1.txt", "rb") as handle:
+        return xmlrpclib.Binary(handle.read())
+
+
+
+
+server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
+print "Listening on port 8000 ... "
+server.register_multicall_functions()
+
+server.register_function(SplitElementTxt,"SplitElementTxt")
+server.register_function(datakirim, "datakirim")
+#server.register_function(SortCount,"SortCount")
+server.serve_forever()
