33eb6990d5e2c66e8c020161fb77d66b7eefc9a5
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-03-31 01:44:24 +0800

    sending file ke worker sukses

--- PublicSubcibe/receive_logs.py
+++ PublicSubcibe/receive_logs.py
@@ -3,15 +3,13 @@ __author__ = 'Indra Gunawan'
 import pika
 import sys
 
-#credentials = pika.PlainCredentials('test', 'test')
-#parameters = pika.ConnectionParameters('192.168.0.24', credentials=credentials)
 
+#credentials = pika.PlainCredentials('test', 'test')
 #connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.24', credentials=credentials))
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs',
-                         type='fanout')
+channel.exchange_declare(exchange='logs',type='fanout')
 
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
@@ -20,14 +18,19 @@ channel.queue_bind(exchange='logs',
                    queue=queue_name)
 
 print ' [*] Waiting for logs. To exit press CTRL+C'
-
+'''
 def callback(ch, method, properties, body):
     print " [x] %r" % (body,)
+'''
 
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
+f=open("crongabung1.txt","wb")
 
+def callback(ch, method, properties, body):
+    f.write(body)
+    f.close()
+
+channel.basic_consume(callback,queue=queue_name,no_ack=True)
+print ' [*] logs diterima'
 try:
     channel.start_consuming()
 
