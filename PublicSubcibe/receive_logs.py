2aaecbb1473bfa237d0452dff18a77981d85f0e8
arijayadwi <arijayadwi@gmail.com>
2016-03-31 01:06:24 +0700

    sudah bisa parsing txt ke publisher dan dihitung

--- PublicSubcibe/receive_logs.py
+++ PublicSubcibe/receive_logs.py
@@ -2,7 +2,8 @@ __author__ = 'Indra Gunawan'
 
 import pika
 import sys
-
+import re
+import collections
 
 #credentials = pika.PlainCredentials('test', 'test')
 #connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.24', credentials=credentials))
@@ -25,11 +26,96 @@ def callback(ch, method, properties, body):
 
 f=open("crongabung1.txt","wb")
 
+temp3=[]
+tempc=[]
+tempoftemp = []
+tempoftimec = []
+hit = 0
+flag=0
+nama_server="DWI_SERVER"
+#IP_Server="192.168.43.207"
+#PORT_Server=8000
+# folder_hasil_computasi="Hasil_Computasi/"
+def SplitElementTxt(ofile):
+    global folder_hasil_computasi, flag, temp3, hit, tempc, tempoftemp, tempoftimec
+    temp1=[]
+    temp_count=[]
+    #folder_log="Log/"
+
+    #ofile = folder_log + ofile
+    buka = open(ofile)
+    for i, line in enumerate(buka):
+        lol = re.split("\W+", line, 8)
+        temp1.append('(' + lol[8])
+    # f = open(folder_hasil_computasi + "cron-copy.txt", 'wb')
+    f = open("cron-copy.txt", 'wb')
+    f.writelines(temp1)
+    buka.close()
+    temp2=[]
+    temp_count=[]
+    # with open(folder_hasil_computasi + "cron-copy.txt") as infile:
+    with open("cron-copy.txt") as infile:
+        counts = collections.Counter(l.strip() for l in infile)
+    for line, count in counts.most_common():
+        temp2.append(line)
+        temp_count.append(count)
+        # return line, count
+    infile.close()
+    f.close()
+    #tempoftemp.append([temp2, temp_count])
+    #buka2 = open(ofile)
+
+    if hit == 0 :
+        temp3 = temp2
+        tempc = temp_count
+        hit=hit +1
+    else :
+        tempoftemp = temp2
+        tempoftempc = temp_count
+        hit=hit +1
+
+    iter1 = 0
+    iter2 = 0
+    if hit>1 :
+        lentemp = len(tempoftemp)
+        lentemp3 = len(temp3)
+
+        cek = 0
+        for i in range(lentemp):
+            for j in range(lentemp3):
+                cek+=1
+                if tempoftemp[i] == temp3[j]:
+                    tempc[j] += tempoftempc[i]
+                    cek = -10;
+                if cek==lentemp3-1 :
+                    temp3.append(tempoftemp[i])
+                    tempc.append(tempoftempc[i])
+            cek = 0
+
+    buka2 = open(ofile)
+    fmt = '%-8s%-20s%s'
+    print(fmt % ('',  'Frequent','Command'))
+    fole = open(nama_server, 'w')
+    # fole = open(folder_hasil_computasi +  "server1.txt", 'w')
+    for i, (name, grade) in enumerate(zip(tempc,temp3)):
+        #print(fmt % (i, name, grade))
+        data3 = fmt % (i, name, grade)
+        #print data3
+        fole.write(data3+"\n")
+
+    buka2.close()
+    fole.close()
+
+
 def callback(ch, method, properties, body):
     f.write(body)
     f.close()
+    SplitElementTxt('crongabung1.txt')
+
+
 
 channel.basic_consume(callback,queue=queue_name,no_ack=True)
+
 print ' [*] logs diterima'
 try:
     channel.start_consuming()
