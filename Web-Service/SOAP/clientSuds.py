c65b1c048c174f8d05ebc978876d676c712922f7
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-04-12 14:51:00 +0800

    Debug Server return type, server sudah bisa parsing hasil ranking logg ke client, progress 85%

--- Web-Service/SOAP/clientSuds.py
+++ Web-Service/SOAP/clientSuds.py
@@ -4,6 +4,5 @@ from suds.client import Client
 
 client = Client('http://localhost:8888/Calculator/soap/description')
 
-
-print 'Pembulatan ke atas  : 5.6      =', client.service.ceiler(5.6)
-print 'Hasil count : ', client.service.count('cron')
+hasil = client.service.count('cron')
+print 'Hasil count : ', hasil
