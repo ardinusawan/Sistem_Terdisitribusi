e6f46999af535b880ff7e3da6f915cfad0b51b3e
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-04-13 22:23:24 +0800

    Fungsi bagi selesai dan client log sudah ditambahkan tinggal testing

--- Web-Service/SOAP/clientSuds.py
+++ Web-Service/SOAP/clientSuds.py
@@ -1,42 +1,78 @@
 __author__ = 'Indra Gunawan'
-__author__ = 'Dwi Pratama'
 #C:\Python27\python.exe C:\Python27\Scripts\ladon-2.7-ctl.py testserve serverLadon.py -p 8888
 from suds.client import Client
 import re
 import collections
 
 client = Client('http://localhost:8888/Calculator/soap/description')
-#client2 = Client('http://192.168.0.0:8888/Calculator/soap/description')
+client2 = Client('http://192.168.0.0:8888/Calculator/soap/description')
 
 hasil = client.service.count('cron')
 hasil = client.service.count('cron.1')
-
+hasil = client.service.count('cron.2')
+hasil = client.service.count('cron.3')
+hasil = client.service.count('cron.4')
+hasil = client.service.count('cron.5')
+hasil = client.service.count('cron.6')
+hasil = client.service.count('cron.7')
+hasil = client.service.count('cron.8')
+hasil = client.service.count('cron.9')
+hasil = client.service.count('cron.10')
+hasil = client.service.count('cron.11')
+hasil = client.service.count('cron.12')
+hasil = client.service.count('cron.13')
+hasil = client.service.count('cron.14')
+hasil = client.service.count('cron.15')
+hasil = client.service.count('cron.16')
+print "[v] response dari server 1 diterima"
+hasil2 = client2.service.count('cron.17')
+hasil2 = client2.service.count('cron.18')
+hasil2 = client2.service.count('cron.19')
+hasil2 = client2.service.count('cron.20')
+hasil2 = client2.service.count('cron.21')
+hasil2 = client2.service.count('cron.22')
+hasil2 = client2.service.count('cron.23')
+hasil2 = client2.service.count('cron.24')
+hasil2 = client2.service.count('cron.25')
+hasil2 = client2.service.count('cron.26')
+hasil2 = client2.service.count('cron.27')
+hasil2 = client2.service.count('cron.28')
+hasil2 = client2.service.count('cron.29')
+hasil2 = client2.service.count('cron.30')
+hasil2 = client2.service.count('cron.31')
+hasil2 = client2.service.count('cron.32')
+hasil2 = client2.service.count('cron.33')
+print "[v] response dari server 2 diterima"
 #hasil1 = client2.service.count('cron.3')
 #print 'Hasil count : ', hasil
+bagi4 = []
+bagi3 = []
+def bagi (hasil):
+    bagi = hasil.split("[")
+    bagi2 = bagi[1].split("]")
+    bagi3 = bagi2[0].split(",")
+    #print bagi
+    #print bagi[1]
+    #print bagi[2]
+    #print bagi2[0][0]
+    #print bagi3
+    #print bagi3[0]
+    bagi4 =bagi[2].split(",")
 
-bagi = hasil.split("[")
-bagi2 = bagi[1].split("]")
-bagi3 = bagi2[0].split(",")
-#print bagi
-#print bagi[1]
-#print bagi[2]
-#print bagi2[0][0]
-#print bagi3
-#print bagi3[0]
-bagi4 =bagi[2].split(",")
 
 #print bagi4
 
 #listranking = bagi3
 #listcomand = bagi4
+print "[v] response dari server-server mulai diranking"
 tempc1 = []
 temp1 = []
 tempc2 = []
 temp2 = []
-
+bagi(hasil)
 temp1 = bagi4
 tempc1 = [int(i) for i in bagi3]
-
+bagi(hasil2)
 temp2 = bagi4
 tempc2 = [int(i) for i in bagi3]
 
@@ -77,6 +113,6 @@ for i, (name, grade) in enumerate(zip(tempc1,temp1)):
         data3 = fmt % (i+1, name, grade)
         print data3
         hitung = hitung +1
-
+print "[v] Done"
 
 
