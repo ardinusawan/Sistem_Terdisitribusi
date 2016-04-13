34d5ca6130aa069bbd75f80eb10cbbad99d15217
ardinusawan <ardi.nusawan13@gmail.com>
2016-04-13 12:55:49 +0700

    yey

---
+++ Web-Service/RESTful/Server.py
@@ -0,0 +1,54 @@
+# Source: http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+import json
+import urllib2
+import os
+import hello
+import SplitElement
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/test', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+# @app.route('/olah_log',methods=['GET'])
+# def olah_log():
+
+@app.route('/hello', methods=['GET'])
+def print_berhasil():
+    hello.write()
+    return "berhasil"
+
+@app.route('/run', methods=['GET'])
+def run():
+    SplitElement.SplitElementTxt("cron")
+    return 'Berhasil';
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5001,debug=True)
+    # app.register_blueprint(printme)
+
